0949791580212bf1445d3d575601de759e578618
Christen <christenborrero@ufl.edu>
2025-11-03 16:39:33 -0500

    finished the last screen

--- main.cpp
+++ main.cpp
@@ -87,16 +87,17 @@ public:
 
         // welcome label
         QLabel *welcomeLabel = new QLabel("Clinical Data Diabetic Analyzer", centralWidget);
-        QFont labelFont = welcomeLabel->font();
-        labelFont.setPointSize(24);
-        welcomeLabel->setFont(labelFont);
+        QFont headingFont = welcomeLabel->font();
+        headingFont.setPointSize(24);
+        welcomeLabel->setFont(headingFont);
         welcomeLabel->setStyleSheet("color : #767676; font-weight: bold;");
 
         // filter label
         QLabel *filterLabel = new QLabel("Filters:", centralWidget);
-        labelFont.setPointSize(18);
-        filterLabel->setFont(labelFont);
-        filterLabel->setStyleSheet("QLabel {color : #B2B2B2}");
+        QFont sHeadingFont = filterLabel->font();
+        sHeadingFont.setPointSize(18);
+        filterLabel->setFont(sHeadingFont);
+        filterLabel->setStyleSheet("color : #B2B2B2;");
 
         // divider
         QFrame *divider = new QFrame();
@@ -149,9 +150,8 @@ public:
 
         // heading
         QLabel *heading = new QLabel("Graphs Based on Data Selected");
-        QFont font = heading->font();
-        font.setPointSize(24);
-        heading->setStyleSheet("font-weight: bold;");
+        heading->setFont(headingFont);
+        heading->setStyleSheet("color : #767676; font-weight: bold;");
 
         // line series
         QLineSeries *series = new QLineSeries();
@@ -173,8 +173,6 @@ public:
         // show page 3 button
         QPushButton *button = new QPushButton("Done", page2);
         button->setMinimumSize(200, 50);
-        buttonFont = button->font();
-        buttonFont.setPointSize(12);
         button->setFont(buttonFont);
         button->setStyleSheet("color : #767676; font-weight: bold;");
 
@@ -192,13 +190,44 @@ public:
 
         //heading
         QLabel *helpHeading = new QLabel("How to Get Help:");
-        QFont helpFont = helpHeading->font();
-        helpFont.setPointSize(24);
-        helpHeading->setFont(helpFont);
-        helpHeading->setStyleSheet("font-weight: bold;");
+        helpHeading->setFont(headingFont);
+        helpHeading->setStyleSheet("color : #767676; font-weight: bold;");
+
+        QLabel *linksSHeading = new QLabel("Links:");
+        linksSHeading->setFont(sHeadingFont);
+        linksSHeading->setStyleSheet("color : #B2B2B2; font-weight: bold;");
+
+        QLabel *link = new QLabel("<a href = \"https://www.redcross.org/about-us/news-and-events/news/2025/diabetes-month--busting-myths--saving-lives.html?srsltid=AfmBOoqMTT_ojPT9rooqXcW0F9jhzdG5rQ9TrjiAL6D3zQuWBrg4rpm7\">Link Example</a>");
+        link->setTextInteractionFlags(Qt::TextBrowserInteraction);
+        link->setOpenExternalLinks(true);
+
+        QLabel *prevSHeading = new QLabel("Steps to Prevention:");
+        prevSHeading->setFont(sHeadingFont);
+        prevSHeading->setStyleSheet("color : #B2B2B2; font-weight: bold;");
+
+        QFrame *paragraph = new QFrame;
+        paragraph->setFrameShape(QFrame::Box);
+        paragraph->setFrameShadow(QFrame::Plain);
+        paragraph->setLineWidth(1);
+        paragraph->setStyleSheet("background-color: #B2B2B2; border-radius: 15px;");
+
+        QVBoxLayout *paragraphLayout = new QVBoxLayout;
+
+        QLabel *prevParagraph = new QLabel(
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer placerat varius massa semper ultrices. Sed ipsum ipsum, aliquet in lorem vel, scelerisque tempor arcu. Nunc in congue felis. Vestibulum sit amet dui eget arcu dignissim dictum. Proin ante turpis, sodales ut lacus eget, suscipit gravida sapien. Morbi vel odio volutpat, ultrices justo vitae, eleifend lacus. Duis a tempus turpis. Proin sit amet sapien non quam pulvinar posuere. Pellentesque volutpat quis orci in bibendum."
+            "Donec tempor, nulla non venenatis ultrices, dolor mauris scelerisque eros, ornare eleifend ex turpis eu dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus finibus, justo nec accumsan auctor, turpis justo maximus orci, a blandit neque odio quis quam. Proin sollicitudin nunc lacus, vitae commodo lectus posuere vitae. Sed elementum venenatis turpis, eu consequat velit imperdiet id. Phasellus auctor lacus fermentum diam pretium, suscipit varius massa ullamcorper. "
+            "<br> Prevention is important because ... Maecenas eu aliquet leo. In mattis eget urna vel pharetra. Praesent sit amet pellentesque nibh. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla sed arcu volutpat, interdum elit ut, condimentum nisi. Mauris ac tristique nisi. Nullam posuere eros volutpat lacus finibus vestibulum. Morbi iaculis leo vitae lectus accumsan, id molestie sem consequat. Nulla in mauris eu velit facilisis accumsan in eget dolor. Donec maximus bibendum eros "
+            "vel molestie. Proin pharetra nec diam id pretium. Pellentesque nec sem eget nisi ullamcorper congue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed ornare tortor a nisi viverra interdum. Praesent at nunc pulvinar, dignissim dolor quis, lacinia leo. Morbi ligula ligula, ultricies nec tellus non, volutpat elementum metus. Maecenas ac diam sit amet tortor auctor facilisis et a purus. Sed fringilla maximus turpis, vel venenatis massa. ");
+        prevParagraph->setWordWrap(true);
+        paragraph->setLayout(paragraphLayout);
+        paragraphLayout->addWidget(prevParagraph);
 
         //page 3 layout
         layout3->addWidget(helpHeading, 0, Qt::AlignCenter);
+        layout3->addWidget(linksSHeading, 0, Qt::AlignCenter);
+        layout3->addWidget(link, 0, Qt::AlignCenter);
+        layout3->addWidget(prevSHeading, 0, Qt::AlignCenter);
+        layout3->addWidget(paragraph, 0, Qt::AlignCenter);
         page3->setLayout(layout3);
 
 
@@ -227,7 +256,7 @@ public:
 
         // Set window properties
         resize(550, 600);
-        setWindowTitle("Welcome");
+        setWindowTitle("Diabetic Analyzer");
     }
 
 private:
